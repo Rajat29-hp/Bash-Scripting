dcbad7bb36d654962a2631e1103c3d9ecbc92347
RAJAT KUMAR <kumar.rajat0978@gmail.com>
2025-03-20 08:46:40 +0530

    Create if-else .sh file

---
+++ if-else.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+ count= 10
+ if ["$count" -gt 10 ]
+ then
+       echo "this is true statement"
+else 
+       echo "this is false statement"
+fi
+
+######################################################
+
+#! /bin/bash
+ count= 10
+ if ["$count" -ne 10 ]
+ then
+       echo "this is true statement"
+else 
+       echo "this is false statement"
+fi
+
+######################################################
+
+#! /bin/bash
+ count= 10
+ if ["$count" -eq 10 ]
+ then
+       echo "this is true statement"
+else 
+       echo "this is false statement"
+
+fi
